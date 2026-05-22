446343cc24326ccec7db489136f2f76892ed4a5a
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-22 10:00:47 -0600

    feat: agregué tarea 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+Registro de ventas
+integrante 1*/
+int [] precios = new int[10];
+int [] cantidades = new int[10];   
+string [] productos = new string[10];
+int totalProductos = 0;
