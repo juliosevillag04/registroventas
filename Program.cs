ba511889fc8285d2f4d1896663f4334449e86dfd
juliosevillag04 <jjsevilla@uamv.edu.ni>
2026-05-22 10:20:30 -0600

    feat: variables nombres correctos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿/*
-Registro de ventas
+﻿/*Registro de ventas
 integrante 1*/
-int maxProductos = 10;
-string[] nombres = new string[maxProductos];
-double[,] datosVentas = new double[maxProductos, 2];
-int productosRegistrados = 0;
+int [] precios = new int[10];
+int [] cantidades = new int[10];   
+string [] productos = new string[10];
+int totalProductos = 0;
